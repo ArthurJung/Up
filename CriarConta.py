549eb4a686227dc258d9737321d673af630966bf
ArthurJung <jung@discente.ufg.br>
2022-04-02 21:01:36 -0300

    Pequenas alterações na função CriarConta e nos htmls

--- CriarConta.py
+++ CriarConta.py
@@ -3,18 +3,18 @@ import json
 
 def AlredyExistName_Email(nomeUsuario, email):
     with open('Contas.json', 'r') as file:
-        _accounts = json.load(file)
-        for account in _accounts['_accounts']:
-            if account['_nomeUsuario'] == nomeUsuario:
+        accounts = json.load(file)
+        for account in accounts['_accounts']:
+            if account['_nome'] == nomeUsuario:
                 return True
-            if account['_perfil']['_email'] == email:
+            if account['_email'] == email:
                 return True
         else:
             return False
 
 
 def CriarConta(account):
-    account.PerfilToJSON()
+    account.ToJSON()
     account = json.loads(json.dumps(account.__dict__))
     with open('Contas.json', 'r') as file:
         data = json.load(file)
