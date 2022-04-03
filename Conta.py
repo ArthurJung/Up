b2e6d0297d6ca6ee832d9202c431a6062a48adb5
ArthurJung <jung@discente.ufg.br>
2022-04-02 21:00:00 -0300

    Commit da classe conta mesclada com a classe perfil

--- Conta.py
+++ Conta.py
@@ -1,94 +1,114 @@
-from Perfil import Perfil
 from Postagem import Postagem
 from Conversa import Conversa
+from datetime import datetime
 from GerarID import NewId
 import json
 
 
-class Conta:
-    def __init__(self, _nomeUsuario, _senha, _perfil=None, _email='', _dataNascimento='', _postagens=[], _conversas=[], _dateRemove=0):
-        self._nomeUsuario = _nomeUsuario
+class Conta_Perfil:
+    def __init__(self, _idUsuario, _nome, _senha, _email, _dataNascimento, _fotoPerfil, _postagens, _conversas, _status):
+        self._idUsuario = _idUsuario
+        self._nome = _nome
         self._senha = _senha
-        if _perfil is None:
-            self._perfil = Perfil(NewId(), _nomeUsuario, _email, _dataNascimento)
-            self._postagens = []
-        else:
-            self._perfil = Perfil(**_perfil)
-            self._postagens = []
-            for post in _postagens:
-                self._postagens.append(Postagem(**post))
-        self._conversas = _conversas
-        self._dateRemove = _dateRemove
+        self._email = _email
+        self._dataNascimento = _dataNascimento
+        self._fotoPerfil = _fotoPerfil
+        self._postagens = [Postagem(**post) for post in _postagens]
+        self._conversas = [Conversa(**conversa) for conversa in _conversas]
+        self._status = _status
 
     @property
-    def perfil(self):
-        return self._perfil
+    def idUsuario(self):
+        return self._idUsuario
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def dataNascimento(self):
+        return self._dataNascimento
+
+    @property
+    def fotoPerfil(self):
+        return self._fotoPerfil
 
     @property
     def postagens(self):
         return self._postagens
 
-    def EditarPerfil(self, nome=None, email=None, dataNascimento=None, fotoURL=None):
-        if nome is not None:
-            self._perfil.nome = nome
-        if email is not None:
-            self._perfil.email = email
-        if dataNascimento is not None:
-            self._perfil.dataNascimento = dataNascimento
-        if nome is not None:
-            self._perfil.fotoURL = fotoURL
+    @property
+    def conversas(self):
+        return self._conversas
 
-    def RemoverPerfil(self, nomeUsuario, senha):
-        from datetime import datetime
-        if (nomeUsuario == self._nomeUsuario) and (senha == self._senha):
-            self._perfil.showStatus = False
-            self._dateRemove = datetime.today()
-            # Função do banco de dados, apagar depois do dateRemove ultrapassar 30 dias
+    @property
+    def status(self):
+        return self._status
 
-    def RecuperarPerfil(self, nomeUsuario, senha):
-        if (nomeUsuario == self._nomeUsuario) and (senha == self._senha):
-            self._perfil.showStatus = True
-            self._dateRemove = 0
+    def EditarPerfil(self, nome, senha, email, dataNascimento, fotoPerfil):
+        self._nome = nome
+        self._senha = senha
+        self._email = email
+        self._dataNascimento = dataNascimento
+        self._fotoPerfil = fotoPerfil
 
-    def FazerPostagem(self, descricao, fotoURL=[]):
-        post = Postagem(NewId(), self._perfil.idUsuario, descricao, 0, fotoURL)
+    def RemoverPerfil(self, nome, senha):
+        if (nome == self._nome) and (senha == self._senha):
+            self._status = False
+            # Função do banco de dados, apagar se o status for False por mais de 30 dias
+
+    def RecuperarPerfil(self, nome, senha):
+        if (nome == self._nome) and (senha == self._senha):
+            self._status = True
+
+    def FazerPostagem(self, descricao, fotos):
+        post = Postagem(NewId(), self._idUsuario, descricao, fotos, datetime.today(), [], 0)
         self._postagens.append(post)
-
-        post.FotoToJSON()
-        post = json.loads(json.dumps(post.__dict__))
+        post.ToJSON()
 
         with open('Contas.json', 'r') as file:
-            data = json.load(file)
-            for account in data['_accounts']:
-                if account['_nomeUsuario'] == self._nomeUsuario:
-                    account['_postagens'].append(post)
+            accounts = json.load(file)
+            for account in accounts['_accounts']:
+                if account['_nome'] == self._nome:
+                    account['_postagens'].append(json.loads(json.dumps(post.__dict__)))
         with open('Contas.json', 'w') as file:
-            json.dump(data, file)# Bizarrice
+            json.dump(accounts, file)
+        from Login import Login
+        return Login(self._nome, self._senha)
 
     def RemoverPostagem(self, idPostagem):
         for postagem in self._postagens:
             if postagem.idPostagem == idPostagem:
                 self._postagens.remove(postagem)
-                break
+                return 1 # postagem removida
+        else:
+            return 0 # Erro ao remover postagem
 
-    def CriarConversa(self, idParticipante, texto, fotoURL=None):
-        self._conversas.append(Conversa(self._perfil.idUsuario, idParticipante, texto, fotoURL))
+    def CriarConversa(self, idParticipantes, texto):
+        conversation = Conversa(NewId(), self._idUsuario, idParticipantes, texto)
+        self._conversas.append(conversation)
 
     def SairConversa(self, idConversa):
         for conversa in self._conversas:
             if conversa.idConversa == idConversa:
-                conversa.SelfRemoveConversa(self._perfil.idUsuario)
                 self._conversas.remove(conversa)
+                return 1  # conversa removida
+        else:
+            return 0  # Erro ao remover conversa
 
-    def DarLike(self, postagem):
-        postagem.likeUser(self.perfil.idUsuario)
+    # def DarLike(self, postagem):
+    #     postagem.likeUser(self._idUsuario)
 
-    def PerfilToJSON(self):
-        self._perfil.FotoToJSON()
-        self._perfil = json.loads(json.dumps(self._perfil.__dict__))
-
-    # def PostagemToJSON(self):
-    #     if self._postagens is not None:
-    #         for post in self._postagens:
-    #             post.FotoToJSON()
-    #         self._postagens = json.loads(json.dumps(self._postagens.__dict__))
+    def ToJSON(self):
+        for post in self._postagens:
+            post.ToJSON()
+        for conversa in self._conversas:
+            conversa.ToJSON()
