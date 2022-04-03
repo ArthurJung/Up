de069d761a6e2193c05296b9140592a0887482b3
ArthurJung <jung@discente.ufg.br>
2022-04-02 21:00:57 -0300

    Excluindo os arquivos feed, Foto e perfil

--- Feed.py
+++
@@ -1,9 +0,0 @@
-class Feed:
-    def __init__(self, contas):
-        # self._postagens = []# Banco de Dados de postagens
-        self.showFeed(contas)
-
-    def showFeed(self, contas):
-        for conta in contas:
-            for post in conta.postagens:
-                print(f'\n{post.descricao}')
