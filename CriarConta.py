b94269fbe01b42bc2e7b7cad14a3de3e03a2e101
Maykon Adriell <maykonadriell@discente.ufg.br>
2022-04-18 16:16:12 -0300

    revisão2.1

--- CriarConta.py
+++ CriarConta.py
@@ -3,11 +3,11 @@ import json
 # Verifica se já existe uma conta com o mesmo nome de usuário ou email
 def AlredyExistName_Email(nomeUsuario, email):
 
-    #busca no banco de dados JSON
+    #busca no banco de dados Json
     with open('Contas.json', 'r') as file:
         accounts = json.load(file)
 
-        #Se a conta já existir retorna True, se ainda não existir retorna False
+        #Se a conta já existir retorna True, do contrario , retorna False
         for account in accounts['_accounts']:
             if account['_nome'] == nomeUsuario:
                 return True
@@ -16,7 +16,7 @@ def AlredyExistName_Email(nomeUsuario, email):
         else:
             return False
 
-#Cria a conta inserindo as informações e atualiza o banco de dados
+#Cria a conta inserindo as informações e as atualiza no banco de dados
 def CriarConta(account):
     account.ToJSON()
     account = json.loads(json.dumps(account.__dict__))
